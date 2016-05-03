00d2ff02d89af9e1d97dc75665ba41d32e4d0c31
neworderofjamie <james.knight@manchester.ac.uk>
2016-05-03 17:47:32 +0100

    Actually we need neuron model with CA2 adaption AND dual exponential synapses

--- pynn_spinnaker_bcpnn/runtime/build/if_curr_ca2_adaptive_dual_exp/config.h
+++ pynn_spinnaker_bcpnn/runtime/build/if_curr_ca2_adaptive_dual_exp/config.h
@@ -2,10 +2,10 @@
 
 // Model includes
 #include "neuron_processor/input_buffer.h"
-#include "neuron_processor/synapse_models/exp.h"
 
 // IF curr ca2 adaptive module includes
 #include "ca2_adaptive.h"
+#include "dual_exp.h"
 
 #include "../../bcpnn_intrinsic.h"
 
@@ -15,7 +15,7 @@ namespace NeuronProcessor
 // Typedefines
 //-----------------------------------------------------------------------------
 typedef ExtraModels::CA2Adaptive Neuron;
-typedef SynapseModels::Exp Synapse;
+typedef ExtraModels::DualExp Synapse;
 typedef BCPNN::BCPNNIntrinsic<13, 6> IntrinsicPlasticity;
 
 typedef InputBufferBase<uint32_t> InputBuffer;
