2ebc1416f49ab5397f56b459a15f0b511a639e72
neworderofjamie <james.knight@manchester.ac.uk>
2016-05-13 10:08:14 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -92,6 +92,6 @@ setup(
 
     # Requirements
     install_requires=["pynn>=0.8", "rig>=1.1.0, <2.0.0",
-                      "bitarray>=0.8.1, <1.0.0", "pynn_spinnaker>=0.1"],
+                      "bitarray>=0.8.1, <1.0.0", "pynn_spinnaker>=0.2"],
     zip_safe=False,  # Partly for performance reasons
 )
